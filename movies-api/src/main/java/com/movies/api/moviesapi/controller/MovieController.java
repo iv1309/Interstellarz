fe507e85a4c99f5580f6c4b933e6dfccf5ea9ad3
iv1309 <iv1309@g.rit.edu>
2022-03-17 23:04:05 -0400

    a way better api

---
+++ movies-api/src/main/java/com/movies/api/moviesapi/controller/MovieController.java
@@ -0,0 +1,145 @@
+package com.movies.api.moviesapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.movies.api.moviesapi.model.Movie;
+import com.movies.api.moviesapi.persistence.MovieDAO;
+
+/**
+ * Handles the REST API requests for the Product resource
+ * <p>
+ * {@literal @}RestController Spring annotation identifies this class as a REST API
+ * method handler to the Spring framework
+ * 
+ * @author Team 1
+ */
+
+@RestController
+@RequestMapping("movies")
+public class MovieController {
+    private static final Logger LOG = Logger.getLogger(MovieController.class.getName());
+    private MovieDAO movieDao;
+
+    public MovieController(MovieDAO movieDao) {
+        this.movieDao = movieDao;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovie(@PathVariable int id) {
+        LOG.info("GET /movies/" + id);
+        try {
+            Movie movie = movieDao.getMovie(id);
+            if (movie != null)
+                return new ResponseEntity<Movie>(movie,HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Movie[]> getMovies() {
+        LOG.info("GET /movies");
+
+        // Replace below with your implementation
+        try{
+            Movie movie[] = movieDao.getMovies();
+            if(movie != null){
+                return new ResponseEntity<Movie[]>(movie,HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Movie[]> searchMovies(@RequestParam String name) {
+        LOG.info("GET /movies/?name="+name);
+
+        // Replace below with your implementation
+        try{
+            Movie movie[] = movieDao.findMovies(name);
+            if(movie != null){
+                return new ResponseEntity<Movie[]>(movie,HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch(IOException e){
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        LOG.info("POST /movies " + movie);
+
+        // Replace below with your implementation
+        try{
+            movie = movieDao.createMovie(movie);
+            if(movie != null){
+                return new ResponseEntity<Movie>(movie,HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch(Exception e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Movie> updateHero(@RequestBody Movie movie) {
+        LOG.info("PUT /movies " + movie);
+
+        // Replace below with your implementation
+        try{
+            movie = movieDao.updateMovie(movie);
+            if(movie != null){
+                return new ResponseEntity<Movie>(movie,HttpStatus.OK);
+            }else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch(Exception e) {
+        LOG.log(Level.SEVERE,e.getLocalizedMessage());
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Movie> deleteMovie(@PathVariable int id) {
+        LOG.info("DELETE /movies/" + id);
+
+        // Replace below with your implementation
+        try{
+            if(movieDao.deleteMovie(id)){
+                return new ResponseEntity<>(HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch(Exception e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ movies-api/src/main/java/com/movies/api/moviesapi/persistence/MovieDAO.java
@@ -0,0 +1,21 @@
+package com.movies.api.moviesapi.persistence;
+
+import java.io.IOException;
+
+import com.movies.api.moviesapi.model.Movie;
+
+public interface MovieDAO {
+
+    Movie[] getMovies() throws IOException;
+
+    Movie[] findMovies(String containsText) throws IOException;
+
+    Movie getMovie(int id) throws IOException;
+
+    Movie createMovie(Movie movie) throws IOException;
+
+    Movie updateMovie(Movie movie) throws IOException;
+
+    boolean deleteMovie(int id) throws IOException;
+
+}
